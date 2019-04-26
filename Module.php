11520b4d0191f630fb1ac75c3bdce1c81b962b79
nadine-shi <nadine@afterlogic.com>
2019-04-26 14:48:12 +0300

    License and other texts improving

--- Module.php
+++ Module.php
@@ -8,6 +8,8 @@
 namespace Aurora\Modules\MailAuthCpanel;
 
 /**
+ * This module allows cPanel user to Access Webmail from Email Accounts screen.
+ * 
  * @license https://www.gnu.org/licenses/agpl-3.0.html AGPL-3.0
  * @license https://afterlogic.com/products/common-licensing Afterlogic Software License
  * @copyright Copyright (c) 2019, Afterlogic Corp.
